27ddced58b616fba777d85639fab01824865c1d0
Odu-Enkay <[erozonankiru@gmail.com]>
2025-04-11 05:31:47 -0600

    added userInput function

--- client.js
+++ client.js
@@ -1,5 +1,10 @@
 // establishes a connection with the game server
 const net = require("net");
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
 
 const connect = function () {
   const conn = net.createConnection({
@@ -7,12 +12,20 @@ const connect = function () {
     port: 50541// PORT number here,
 });
   conn.on('data', (data) => {
-    console.log('Server says: ', data);
+    console.log('Move: down ', data);
 });
+
+  rl.on('line', (input) => {
+  // console.log(`Received: ${input}`);
+    connection.write(input)
+  });
+
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
-  return conn.on;
+  return conn;
 };
+
+
 
 module.exports = connect;
